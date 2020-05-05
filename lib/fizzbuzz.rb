29c7a12c3c8aee37cf90a431d29b8fec8710c2cc
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-05-05 15:14:25 +0000

    final version

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,13 @@
-def fizzbuzz (number)
+def fizzbuzz(number)
 
   if number % 3 == 0 && number % 5 == 0
-    puts 'FizzBuzz'
+    'FizzBuzz'
   elsif number % 3 == 0
-    puts 'Fizz'
+    'Fizz'
   elsif number % 5 == 0
-    puts 'Buzz'
+    'Buzz'
   else 
-    puts number
+    number
   end
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,35 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do 
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns the number if number is not a multiple of 3 or 5' do
+    expect(fizzbuzz(2)).to eq 2
   end
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+  it 'returns "Fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+  it 'returns "Buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+  it 'returns "Fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'Fizz'
+  end
+  it 'returns "Fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'Fizz'
+  end
+  it 'returns "Buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'Buzz'
+  end
+  it 'returns "Fizz" when passed 12' do
+    expect(fizzbuzz(12)).to eq 'Fizz'
+  end
+  it 'returns "FizzBuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+  it 'returns "Fizz" when passed 18' do
+    expect(fizzbuzz(18)).to eq 'Fizz'
+  end
+  it 'returns "Buzz" when passed 20' do
+    expect(fizzbuzz(20)).to eq 'Buzz'
   end
 end
 
