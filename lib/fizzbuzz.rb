541c7b3960c92e96c927f16faa0e8dc99874496f
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-05 15:50:47 +0100

    Updated fizzbuzz version

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
 def fizzbuzz (number)
-  'fizz'
+
+  if number % 3 == 0 && number % 5 == 0
+    puts 'FizzBuzz'
+  elsif number % 3 == 0
+    puts 'Fizz'
+  elsif number % 5 == 0
+    puts 'Buzz'
+  else 
+    puts number
+  end
+
 end
